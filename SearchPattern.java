93b0519c2fb1e2c199b9abef2e80577c9e3cf8b5
Name <vishwaas7saxena@gmail.com>
2020-12-19 22:09:20 +0530

    Striong pattern search naive approach

---
+++ SearchPattern.java
@@ -0,0 +1,24 @@
+public class SearchPattern {
+    public static void main(String[] args) {
+        String txt = "AABAACAADAABAAABAA";
+        String pat = "AABA";
+        NaiveSearch(txt, pat);
+        
+    }
+    public static void NaiveSearch(String str, String pattern){
+        int n=pattern.length();
+        int m=str.length();
+        for(int i=0;i<m-n;i++){
+            int j;
+            for(j=0;j<n;j++){
+                if(str.charAt(i+j)!=pattern.charAt(j))
+                break;
+                if(j==n-1){
+                    System.out.println("Pattern found at index "+i);
+                }
+            }
+        }
+
+    }
+    
+}
